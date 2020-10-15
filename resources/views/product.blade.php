2feba0334f9a9b87764496830de447eaf89b8baf
Jose <barbozagonzalesjose@gmail.com>
2020-10-15 13:53:54 -0500

    carousel products

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -55,12 +55,44 @@
                         </div>
                       </div>
                       <div class="row">
-                        <div class="col-6">
-                          @if ($images)
+                        <div class="col-12 col-md-10 offset-md-1">
+                          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                              {{-- <div class="carousel-item active">
+                                      <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/applewatch/watchos7-series5-watch-face-edit-complication.jpg" class="d-block w-100" alt="...">    
+                              </div> --}}
+                              <?php $item = 0 ?>
+                                @if ($images)
+                                  @foreach ($images as $image)
+                                    @if ($item == 0)
+                                      <div class="carousel-item active">
+                                        <img id="showProduct" src="{{ $image->get_image }}" class="d-block w-100" alt="...">    
+                                      </div>    
+                                    @else
+                                      <div class="carousel-item">
+                                        <img id="showProduct" src="{{ $image->get_image }}" class="d-block w-100" alt="...">    
+                                      </div>    
+                                    @endif
+                                    <?php $item++ ?>                                   
+                                  @endforeach 
+                                @endif
+
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                  {{--         @if ($images)
                             @foreach ($images as $image)
                               <img src="{{ $image->get_image }}" class="card-img-top" alt="{{ $image->id }}">
                             @endforeach     
-                          @endif
+                          @endif --}}
                         </div>
                       </div>
                       {{-- section Comentary --}}
