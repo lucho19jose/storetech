71ae684cc0ca4cb67796d46d68777bac8cce16e5
Jose <barbozagonzalesjose@gmail.com>
2020-10-16 19:38:42 -0500

    add favicon :)

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -8,13 +8,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ URL::asset('css/stylewhatsapp.css') }}">
-    <title>@yield('titulo')</title>
+    <link  rel="icon"   href="https://drive.google.com/uc?id=1srTWsG4jI1rXFVBwtAy_W0hLtYPRDDJl" type="image/png" />
+    <title>AndahuaylasB2B</title>
 </head>
 <body>
     <h1 class="text-center">B2B Andahuaylas</h1>
     {{-- link whatsapp --}}
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <a href="https://api.whatsapp.com/send?phone=972742928&text=https://api.whatsapp.com/send?phone=972742928&text=Hola!%20quetal%20b2b" class="float" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=51972742928&text=https://api.whatsapp.com/send?phone=972742928&text=Hola!%20quetal%20b2b" class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
         </a>
     @include('layouts.partials.header')
